8151ab7cce1311a6e6a63533047bfa7d631bf02e
Anjali Gupta <110262694+anjali6387@users.noreply.github.com>
2024-08-26 16:04:31 +0530

    Update requestMethods.js

--- ecomapp/src/requestMethods.js
+++ ecomapp/src/requestMethods.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/";
+// const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "https://ecommerse-mern-backend.onrender.com/";
 
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
